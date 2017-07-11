ea7366c181ba317e3006c4b36369d7f9de94332c
Jignesh <Jignesh-81726@users.noreply.github.com>
2017-07-11 14:16:09 +0530

    Create Solution.java

---
+++ Algorithms/Warmup/A Very Big Sum/Solution.java
@@ -0,0 +1,22 @@
+/*  
+ *      DeveloperName(): Jignesh Chudasama
+ *      GithubName(): https://github.com/Jignesh-81726
+ */
+ 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int n  = input.nextInt();
+        long sum = 0;
+        for(int i = n; i>0; i--)
+        {
+            sum += input.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
